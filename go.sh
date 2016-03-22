564279c816c92a9debeb650c1903ea47af388c98
jimmyLin <jimmyLin@gmain.com>
2016-03-22 13:31:03 +0800

    mod

--- go.sh
+++ go.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+cp kernel/.config /ftp/kernel.config
+cp .config /ftp/config.config
 make V=99
 cp bin/ramips/openwrt-ramips-mt7620a-mt7620a_mt7610e-initramfs-uImage.bin /ftp/
 ls /ftp/ -l
